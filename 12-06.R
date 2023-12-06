83a56b0e2db56e0ab4fc4271c4b09d63f42bdf7e
Wei-Hao, Chen <145751233+Howard8910@users.noreply.github.com>
2023-12-06 14:49:54 +0800

    12-06

---
+++ 12-06.R
@@ -0,0 +1,8 @@
+oneComingStudentMajor <- sample(c("Economics", "Sociology"), 1)
+print(oneComingStudentMajor)
+
+if (oneComingStudentMajor == "Economics"){
+  print("Please go to room 201.")
+}else{
+  print("Please go to room 301.")
+}
